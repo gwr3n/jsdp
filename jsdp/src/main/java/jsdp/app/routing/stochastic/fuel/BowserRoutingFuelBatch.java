8f1420df06aaf89695d5a87d9b333f9475b827db
gwr3n <robros@gmail.com>
2016-08-13 12:38:45 +0200

    New instance batch

--- jsdp/src/main/java/jsdp/app/routing/stochastic/fuel/BowserRoutingFuelBatch.java
+++ jsdp/src/main/java/jsdp/app/routing/stochastic/fuel/BowserRoutingFuelBatch.java
@@ -64,7 +64,7 @@ public class BowserRoutingFuelBatch {
       tankCapacity = new int[]{10,10,10};
       
       final int minFuelConsumption = 0;
-      final int maxFuelConsumption = 4;
+      final int maxFuelConsumption = 5;
       
       /**
        * Variable parameters
@@ -77,12 +77,12 @@ public class BowserRoutingFuelBatch {
           {3,3,3,3,3},
          },
          {{2,2,2,2,2},
-          {5,5,5,5,5},
-          {8,8,8,8,8},
+          {1,1,1,1,1},
+          {3,3,3,3,3},
          },
-         {{1,2,3,4,5},
-          {5,4,3,2,1},
-          {5,5,0,0,3},
+         {{0,1,2,3,4},
+          {4,3,2,1,0},
+          {3,3,0,0,2},
          }
       };
       int[] fuelStockOutPenaltyCosts = {100,500};
