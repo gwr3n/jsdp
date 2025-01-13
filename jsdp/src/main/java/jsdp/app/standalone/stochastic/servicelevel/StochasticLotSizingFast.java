dc7656271d0d4c2163f344a2fcd155bbb3a9415f
Roberto Rossi <robros@gmail.com>
2025-01-13 21:58:02 +0000

    Added comment

--- jsdp/src/main/java/jsdp/app/standalone/stochastic/servicelevel/StochasticLotSizingFast.java
+++ jsdp/src/main/java/jsdp/app/standalone/stochastic/servicelevel/StochasticLotSizingFast.java
@@ -1132,7 +1132,7 @@ class PoissonMultiperiodNewsboy extends MultiperiodNewsboy {
       for(int i = 0; i < demand.length; i++) {
          cumDemand += demand[i].getMean();
          PoissonLossFunction lf = new PoissonLossFunction(cumDemand);
-         //NormalLossFunction lf = new NormalLossFunction(cumDemand, Math.sqrt(cumDemand));
+         //NormalLossFunction lf = new NormalLossFunction(cumDemand, Math.sqrt(cumDemand)); // Normal approximation to Poisson
          etc += this.h*lf.Lc(x) + this.p[i]*lf.L(x); 
       }
       return etc;
